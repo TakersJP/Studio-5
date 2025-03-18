9e7b570401f0c4956d6a36c481dc009bd3023051
TakersJP <97802658+TakersJP@users.noreply.github.com>
2025-03-18 16:47:28 -0700

    Added the current number of lives

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,10 +1,17 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : SingletonMonoBehavior<GameManager>
 {
-    [SerializeField] private int maxLives = 3;
+    [SerializeField] private int startingLives = 3; // starting number of lives
+    private int currentLives; // track current lives
+
     [SerializeField] private Ball ball;
     [SerializeField] private Transform bricksContainer;
+
+    [SerializeField] private List<GameObject> heartObjects; // list of heart GameObjects (instead of just Images)
 
     private int currentBrickCount;
     private int totalBrickCount;
@@ -15,6 +22,8 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         ball.ResetBall();
         totalBrickCount = bricksContainer.childCount;
         currentBrickCount = bricksContainer.childCount;
+        currentLives = startingLives; // initialize current lives
+        UpdateHeartsUI(); // update heart icons when game starts
     }
 
     private void OnDisable()
@@ -34,14 +43,39 @@ public class GameManager : SingletonMonoBehavior<GameManager>
         // add camera shake here
         currentBrickCount--;
         Debug.Log($"Destroyed Brick at {position}, {currentBrickCount}/{totalBrickCount} remaining");
-        if(currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
+        if (currentBrickCount == 0) SceneHandler.Instance.LoadNextScene();
     }
 
     public void KillBall()
     {
-        maxLives--;
+        currentLives--;
         // update lives on HUD here
-        // game over UI if maxLives < 0, then exit to main menu after delay
-        ball.ResetBall();
+        UpdateHeartsUI();
+        // game over UI if currentLives < 1, then exit to main menu after delay
+        if (currentLives <= 0)
+        {
+            SceneHandler.Instance.LoadMenuScene();
+        }
+        else
+        {
+            ball.ResetBall();
+        }
     }
+<<<<<<< Updated upstream
+=======
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        scoreCounter.UpdateScore(score);
+    }
+
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < heartObjects.Count; i++)
+        {
+            heartObjects[i].SetActive(i < currentLives);
+        }
+    }
+>>>>>>> Stashed changes
 }
